0db55d645104017ad775aa4bbfeb13d6d25605bd
cuihaiying <cuihaiying@cncic.chemchina.com>
2018-10-26 15:50:40 +0800

    配置wxframework的版本兼容性

--- README.md
+++ README.md
@@ -1,2 +1,104 @@
-# eros-plugin-alicloud-push2
-基于Eros框架下的阿里云推送的集成，支持基础依赖包wxframework > 1.0.9 的版本
+# eros-plugin-alicloud-push
+##### 基于Eros框架下的阿里云移动推送Android端的集成
+       1. 可以将接受到的消息或通知内容传递至JS端以便后续的处理
+       2. 可以指定某一消息的内容传递给JS端，其他的接收的消息不会传递
+## Usage
+### 1. Add dependency
+```groovy
+	dependencies {
+	       implementation 'com.github.heynchy:eros-plugin-alicloud-push:0.0.4'
+	}
+```
+### 2. 在Application中初始化移动推送（可参考demo中的位置）
+```java
+       /**
+         * 初始化推送信息（必须设置）
+         * appKey : 该应用在阿里云上的注册参数
+         * appSecret： 该应用在阿里云上的注册参数
+         * 将生成好的appKey 和 appSecret填入对应的位置
+         */
+        AliPushManger.initCloudChannel(this, "appKey", "*appSecret");
+        
+        /**
+         *  设置是否向JS端推送消息（非必须）
+         *
+         *  默认为true--JS端能接受到任何消息通知
+         *  设置为false--JS端不能接受到任何消息通知
+         */
+        AliPushManger.setIsSend(true);
+        
+       /**
+         * 设置制定的消息类型推送至JS端（非必须）
+         *
+         *  PUSH_NOTIFICATION         通知的推送
+         *  PUSH_MESSAGE              消息的推送
+         *  PUSH_NOTIFICATION_OPEN    打开通知
+         *  PUSH_NOTIFICATION_REMOVE  移除通知
+         *  PUSH_NO_ACTION_OPEN       打开无跳转通知的回调
+         *
+         *  设置效果： 只将设置的消息类型传给JS端，其他的消息不会传给JS端
+         */
+        AliPushManger.setSendAction(PUSH_NOTIFICATION);
+```
+##### 注意：初始化必须在Application中进行，否则会报错
+
+## 其他方法说明
+    1. 设置通知栏图标
+ ```java
+    /**
+     * 设置通知栏图标
+     * 设置推送通知栏图标资源Bitmap
+     * 若不调用本接口，默认获取id为R.drawable.alicloud_notification_largeIcon的资源文件
+     * 若没有获取到指定图标文件，取App启动图标
+     */
+    AliPushManger.setNotificationIcon(Bitmap icon);
+ ```
+    2. 设置状态栏的图标
+ ```java
+    /**
+     * 设置状态栏图标
+     * 设置推送状态栏图标资源Id
+     * 若不调用本接口，默认获取id为R.drawable.alicloud_notification_smallIcon的资源文件
+     * 若没有获取到指定资源文件Id，取App启动图标
+     *
+     */
+    AliPushManger.setNotificationStateIcon(int drawableId) ;
+ ```
+
+## Eros框架下的接收消息的位置 js/config/push.js文件中：
+```java
+  /**
+   * 消息推送
+   * options 客户端的所有消息
+   */
+globalEvent.addEventListener('pushMessage', function (options) {
+    /** 
+     *  options 包含内容主要包括  
+     *        String title                  // 标题(消息，通知都有)
+     *        String summary                // 简介（消息的内容，通知的简介）
+     *        String extraStr               // 其他参数
+     *        Map<String, String> extraMap  // 其他参数
+     *        int action                    // 推送的操作(类型)
+     *        String messageId              // 消息/通知的ID
+     */
+})
+```
+
+
+License
+---------
+    Copyright 2018 heynchy
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+
---
+++ wx_ali_cloud_push/src/main/java/com/heyn/erosplugin/wxalicloudpush/manager/AliPushManger.java
@@ -0,0 +1,152 @@
+package com.heyn.erosplugin.wxalicloudpush.manager;
+
+import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.Log;
+
+import com.alibaba.sdk.android.push.CloudPushService;
+import com.alibaba.sdk.android.push.CommonCallback;
+import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory;
+import com.eros.framework.adapter.router.RouterTracker;
+import com.eros.framework.constant.WXEventCenter;
+import com.eros.framework.manager.ManagerFactory;
+import com.eros.framework.manager.impl.ParseManager;
+import com.eros.framework.manager.impl.dispatcher.DispatchEventManager;
+import com.eros.framework.model.BaseEventBean;
+import com.heyn.erosplugin.wxalicloudpush.event.AliPushMessage;
+import com.heyn.erosplugin.wxalicloudpush.util.Constant;
+
+/**
+ * Author: heynchy
+ * Date:   2018/9/28
+ * <p>
+ * Introduce:
+ */
+public class AliPushManger {
+
+    public static boolean IS_SEND = true; // 是否全部推送
+    public static int PUSH_ACTION = -1;   // 推送的类型
+
+    /**
+     * 设置发送消息的类型(通知,消息等),默认全部发送
+     *
+     * @param action
+     */
+    public static void setSendAction(int action) {
+        IS_SEND = false;
+        PUSH_ACTION = action;
+    }
+
+    /**
+     * 设置是否将消息/通知全部回传给前端
+     *
+     * @param send
+     */
+    public static void setIsSend(boolean send) {
+        IS_SEND = send;
+    }
+
+    /**
+     * 初始化云推送通道
+     *
+     * @param context
+     */
+    public static void initCloudChannel(final Context context, String appKey, String appSecret) {
+
+        CloudPushService pushService = PushServiceFactory.getCloudPushService();
+        PushServiceFactory.init(context);
+        pushService.register(context, appKey, appSecret, new CommonCallback() {
+            @Override
+            public void onSuccess(String s) {
+                Log.i(Constant.TAG, "onSuccess: " + s);
+            }
+
+            @Override
+            public void onFailed(String s, String s1) {
+                Log.i(Constant.TAG, "onFailed: " + s);
+            }
+        });
+        createNotificationChannel(context);
+    }
+
+    /**
+     * 针对Android O 的通知渠道做出相关处理
+     *
+     * @param context
+     */
+    private static void createNotificationChannel(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager mNotificationManager = (NotificationManager)
+                    context.getSystemService(Context.NOTIFICATION_SERVICE);
+            // 通知渠道的id
+            String id = "alipush_1";
+            // 用户可以看到的通知渠道的名字.
+            CharSequence name = "notification channel";
+            // 用户可以看到的通知渠道的描述
+            String description = "notification description";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(id, name, importance);
+            // 配置通知渠道的属性
+            mChannel.setDescription(description);
+            // 设置通知出现时的闪灯（如果 android 设备支持的话）
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            // 设置通知出现时的震动（如果 android 设备支持的话）
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            //最后在notificationmanager中创建该通知渠道
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
+    }
+
+    /**
+     * 发送消息/通知信息至JS服务端
+     *
+     * @param message
+     */
+    public static void sendMsgToJs(AliPushMessage message, int action) {
+        if (IS_SEND || PUSH_ACTION == action) {
+            Activity activity = RouterTracker.peekActivity();
+            BaseEventBean eventBean = new BaseEventBean();
+            eventBean.context = activity;
+            eventBean.type = WXEventCenter.EVENT_PUSHMANAGER;
+            eventBean.clazzName = "com.benmu.framework.event.GlobalEvent";
+            ParseManager param = ManagerFactory.getManagerService(ParseManager.class);
+            String json = param.toJsonString(message);
+            eventBean.param = json;
+            ManagerFactory.getManagerService(DispatchEventManager.class).getBus().post
+                    (eventBean);
+        }
+    }
+
+    /**
+     * 设置通知栏图标
+     * 设 置推送通知栏图标资源Bitmap。
+     * 若不调用本接口，默认获取id为R.drawable.alicloud_notification_largeIcon的资源文件；
+     * 若没有获取到指定图标文件，取App启动图标。
+     *
+     * @param icon
+     */
+    public static void setNotificationIcon(Bitmap icon) {
+        CloudPushService pushService = PushServiceFactory.getCloudPushService();
+        pushService.setNotificationLargeIcon(icon);
+    }
+
+    /**
+     * 设置状态栏图标
+     * 设置推送状态栏图标资源Id；
+     * 若不调用本接口，默认获取id为R.drawable.alicloud_notification_smallIcon的资源文件；
+     * 若没有获取到指定资源文件Id，取App启动图标。
+     *
+     * @param drawableId
+     */
+    public static void setNotificationStateIcon(int drawableId) {
+        CloudPushService pushService = PushServiceFactory.getCloudPushService();
+        pushService.setNotificationSmallIcon(drawableId);
+    }
+}
